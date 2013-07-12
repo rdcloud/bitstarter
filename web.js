f97d9624d963475d8eb4df6b445131ed7ee045ea
Raj Durairaj <rdurairaj123@gmail.com>
2013-07-12 05:21:58 +0000

    Modified to read data from file

--- web.js
+++ web.js
@@ -2,8 +2,22 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var data;
+
+try {
+  data = fs.readFileSync('index.html', 'utf8');
+} catch (e) {
+    if (e.code === 'ENOENT') {
+	console.log('File not found!');
+	} else {
+	    throw e;
+}
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data)
 });
 
 var port = process.env.PORT || 5000;
